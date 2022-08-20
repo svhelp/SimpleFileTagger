77f844e893d46c4352b3121a4ed99c468cfaa38b
Aleksandr Kravchenko <svhelpp@gmail.com>
2022-08-20 13:25:57 +0200

    Добавьте файлы проекта.

---
+++ SimpleFileTagger/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleFileTagger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
