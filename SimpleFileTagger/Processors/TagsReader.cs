6b9b13b4ed67c0e13b058e7bcc1cdb19e5270149
Aleksandr Kravchenko <svhelpp@gmail.com>
2022-08-24 21:00:18 +0200

    -added commands to import old format data; - added command to print root dir data;

--- SimpleFileTagger/Processors/TagsReader.cs
+++ SimpleFileTagger/Processors/TagsReader.cs
@@ -1,4 +1,6 @@
-﻿using SimpleFileTagger.Models;
+﻿using DAL;
+using DAL.Entities;
+using SimpleFileTagger.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,88 @@ namespace SimpleFileTagger.Processors
 {
     internal class TagsReader : ProcessorBase
     {
+        static public RootEntity GetDbRootData(string rootPath)
+        {
+            using var context = new TaggerContext();
+
+            return context.Roots.FirstOrDefault(r => r.Path == rootPath);
+        }
+
+        static public void ImportRootDitectory(string rootPath)
+        {
+            using var context = new TaggerContext();
+            var existingTags = context.Tags.AsQueryable().ToList();
+            var existingLocations = new List<LocationEntity>();
+
+            var pathMap = new Dictionary<string, LocationEntity>();
+
+            var directoriesData = GetDirectoryInfoRecoursively(rootPath);
+            var rootDirectoryData = directoriesData.FirstOrDefault();
+            var rootDirectoryPath = rootDirectoryData.Key.Substring(rootPath.Length);
+
+            var rootDirectory = new LocationEntity
+            {
+                Name = rootDirectoryPath,
+                Tags = GetOrCreateTags(existingTags, rootDirectoryData.Value.Tags),
+                Children = new List<LocationEntity>()
+            };
+
+            pathMap.Add(rootDirectoryPath, rootDirectory);
+
+            var root = new RootEntity
+            {
+                Path = rootPath,
+                RootLocation = rootDirectory
+            };
+
+            foreach (KeyValuePair<string, TaggerDirectoryInfo> directory in directoriesData)
+            {
+                var pathParts = directory.Key.Split("/").ToList();
+                var directoryName = pathParts.Last();
+                var parentPath = string.Join("/", pathParts.Take(pathParts.Count - 1));
+
+                var locationData = new LocationEntity
+                {
+                    Name = directoryName,
+                    Tags = GetOrCreateTags(existingTags, directory.Value.Tags),
+                    Children = new List<LocationEntity>()
+                };
+
+                var parent = pathMap[parentPath];
+                parent.Children.Add(locationData);
+
+                pathMap.Add(directory.Key, locationData);
+            }
+            
+            context.Roots.Add(root);
+        }
+
+        private static List<TagEntity> GetOrCreateTags(List<TagEntity> existingTags, List<TagModel> tags)
+        {
+            var tagEntities = new List<TagEntity>();
+
+            foreach(var tag in tags)
+            {
+                var existingTag = existingTags.FirstOrDefault(t => t.Name == tag.Name);
+
+                if (existingTag != null)
+                {
+                    tagEntities.Add(existingTag);
+                    continue;
+                }
+
+                var newTagEntity = new TagEntity
+                {
+                    Name = tag.Name,
+                };
+
+                existingTags.Add(newTagEntity);
+                tagEntities.Add(newTagEntity);
+            }
+
+            return tagEntities;
+        }
+
         static public Dictionary<string, TaggerDirectoryInfo> GetDirectoryInfoRecoursively(string path)
         {
             Dictionary<string, TaggerDirectoryInfo> result = new Dictionary<string, TaggerDirectoryInfo>();
